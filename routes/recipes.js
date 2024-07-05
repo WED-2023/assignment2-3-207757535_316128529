1bb0536d9bc32b50cbfd46bf10da65e24c2c2a9b
Dvir Zandany <127864652+zandanyd@users.noreply.github.com>
2024-07-05 18:49:31 +0300

    commit

--- routes/recipes.js
+++ routes/recipes.js
@@ -6,9 +6,9 @@ router.get("/", (req, res) => res.send("im here"));
 
 
 /**
- * This path returns a full details of a recipe by its id
+ * This path returns a preview of a recipe by its id
  */
-router.get("/:recipeId", async (req, res, next) => {
+router.get("/recipe/preview/:recipeId", async (req, res, next) => {
   try {
     const recipe = await recipes_utils.getRecipePreviewByID(req.params.recipeId);
     res.send(recipe);
@@ -16,15 +16,46 @@ router.get("/:recipeId", async (req, res, next) => {
     next(error);
   }
 });
-// async function testGetRecipeDetails() {
-//   try {
-//       const recipeDetails = await recipes_utils.getRecipeDetails(716429); // Replace with a valid recipe ID
-//       console.log(recipeDetails);
-//   } catch (error) {
-//       console.error('Error fetching recipe details:', error);
-//   }
-// }
 
-// testGetRecipeDetails();
+/**
+ * This path returns a full details of a recipe by its id
+ */
+router.get("/recipe/fullDetaile/:recipeId", async (req, res, next) => {
+  try {
+    const recipe = await recipes_utils.getRecipeFullDetailsByID(req.params.recipeId);
+    res.send(recipe);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/recipe/random", async (req, res, next) => {
+  try {
+    const recipe = await recipes_utils.getRandomRecipePreview(req.params.number);
+    res.send(recipe);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/recipe/search", async (req, res, next) => {
+  try {
+    const { query, cuisine, diet, intolerances, sort, number } = req.query;
+    const recipes = await recipes_utils.searchRecipePreview(query, cuisine, diet, intolerances, sort, number);
+    res.send(recipes);
+  } catch (error) {
+    next(error);
+  }
+});
+async function testGetRecipeDetails() {
+  try {
+      const recipeDetails = await recipes_utils.searchRecipePreview("pasta", "gluten", "gluten", "italian", "calories", 2); 
+      console.log(recipeDetails);
+  } catch (error) {
+      console.error('Error fetching recipe details:', error);
+  }
+}
+
+testGetRecipeDetails();
 
 module.exports = router;
--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -13,12 +13,35 @@ async function getRecipeInformation(recipe_id) {
     return await axios.get(`${api_domain}/${recipe_id}/information`, {
         params: {
             includeNutrition: false,
-            apiKey: process.env.spooncular_apiKey
+            apiKey: process.env.spooncular_apiKey,
+           
         }
-    });
-}
+    });}
 
+async function getRandomRecipes(number) {
+    return await axios.get(`${api_domain}/random`, {
+        params: {
+            includeNutrition: false,
+            instructionsRequired: true,
+            apiKey: process.env.spooncular_apiKey,
+            number: number
+        }
+    });}
 
+async function complexSearch(query, cuisine, diet, intolerances, sort, number ) {
+    return await axios.get(`${api_domain}/complexSearch`, {
+        params: {
+            query: query,
+            cuisine: cuisine,
+            diet: diet,
+            intolerances: intolerances,
+            sort: sort,
+            includeNutrition: false,
+            instructionsRequired: true,
+            apiKey: process.env.spooncular_apiKey,
+            number: number
+        }
+    });}
 
 async function getRecipePreviewByID(recipe_id) {
     let recipe_info = await getRecipeInformation(recipe_id);
@@ -37,8 +60,55 @@ async function getRecipePreviewByID(recipe_id) {
     }
 }
 
+async function getRecipeFullDetailsByID(recipe_id) {
+    let recipe_info = await getRecipeInformation(recipe_id);
+    let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree, extendedIngredients, summary, analyzedInstructions  } = recipe_info.data;
+
+    return {
+        id: id,
+        title: title,
+        readyInMinutes: readyInMinutes,
+        image: image,
+        popularity: aggregateLikes,
+        vegan: vegan,
+        vegetarian: vegetarian,
+        glutenFree: glutenFree,
+        extendedIngredients: extendedIngredients,
+        analyzedInstructions: analyzedInstructions,
+        summary: summary,
+    }}
+
+    async function getRandomRecipePreview(number) {
+        let recipes_info = await getRandomRecipes(number);
+        let recipes = recipes_info.data.recipes;
+    
+        return recipes.map(recipe => {
+            let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree } = recipe;
+            return {
+                id: id,
+                title: title,
+                readyInMinutes: readyInMinutes,
+                image: image,
+                popularity: aggregateLikes,
+                vegan: vegan,
+                vegetarian: vegetarian,
+                glutenFree: glutenFree,
+            };
+        });}
 
 
+    async function searchRecipePreview(query, cuisine, diet, intolerances, sort, number) {
+        let recipes_info = await complexSearch(query, cuisine, diet, intolerances, sort, number);
+        let recipeIds = recipes_info.data.results.map(recipe => recipe.id);
+    
+        let recipePreviews = await Promise.all(recipeIds.map(id => getRecipePreviewByID(id)));
+    
+        return recipePreviews;
+    }
+
+exports.searchRecipePreview = searchRecipePreview
+exports.getRandomRecipePreview = getRandomRecipePreview;
+exports.getRecipeFullDetailsByID = getRecipeFullDetailsByID;
 exports.getRecipePreviewByID = getRecipePreviewByID;
 
 
